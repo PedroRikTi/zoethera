9d68667871f8c482a07c264d55db01b44dbf1f3c
PedroRikTi <112035262+PedroRikTi@users.noreply.github.com>
2026-05-09 00:04:47 +0000

    add a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <h1>
+      Mudança de Mentalidade: É o sentido original, significando repensar, mudar
+      de ideia e adotar uma nova perspectiva. 
+
+      Sentido Teológico(Bíblico):Frequentemente traduzida como "arrependimento", no Novo
+      Testamento, refere-se à conversão, uma transformação interior que leva à
+      mudança de comportamento e alinhamento com a fé. 
+      
+      Psicologia: Representa umprocesso dereforma da psique, uma mudança profunda no modelo mental de um
+      indivíduo.Sinônimos: Transformação, conversão, mudança de atitude,renovação, reconsideração.
+    </h1>
+  );
+}
+
+export default Home;
