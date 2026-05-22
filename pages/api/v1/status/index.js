cea3e481ddd4d4bf388379dfea9b8df57305d53b
PedroRikTi <112035262+PedroRikTi@users.noreply.github.com>
2026-05-22 00:56:46 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,14 +1,31 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Response status ok" });
+  const updateAt = new Date().toISOString();
+
+  const dbVersion = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersion.rows[0].server_version;
+
+  const dbMaxConnections = await database.query("SHOW max_connections;");
+  const dbMaxConnectionValue = dbMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dbOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const dbOpenedConnectionsValue = dbOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnectionValue),
+        opened_connections: dbOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
-
-// Request (requisição) e Response (resposta) formam o mecanismo fundamental de comunicação síncrona na web, baseada no protocolo HTTP. O cliente (ex: navegador) envia uma request pedindo dados ou ações, e o servidor processa e devolve uma response, contendo o resultado (como dados JSON ou páginas HTML) e códigos de status
-// Request (Requisição)Definição: É o pedido que o cliente realiza a um servidor.Anatomia: Contém o método (GET, POST, PUT, DELETE), URL, cabeçalhos (headers) e, às vezes, um corpo (body) com dados.Propósito: Especificar qual ação o cliente quer realizar (ex: buscar um usuário, salvar um produto).
-// Response (Resposta)Definição: É a resposta enviada pelo servidor ao cliente após processar a request.Anatomia: Contém uma linha de status (código HTTP, ex: \(200\) para sucesso, \(404\) para não encontrado), cabeçalhos e o corpo com o conteúdo solicitado.Propósito: Informar o resultado da operação e, se aplicável, enviar os dados requisitados.
-// export default é Um recurso do JavaScript ES6 usado para exportar um único valor, função ou objeto como a exportação principal de um módulo (arquivo).É muito utilizado no React para exportar componentes.
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,13 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-});
 
-//A Fetch API é uma interface JavaScript nativa que permite fazer requisições HTTP de forma assíncrona para buscar dados de servidores. Ela substitui o antigo XMLHttpRequest usando um sistema moderno baseado em Promises, tornando o código de integração com APIs muito mais simples e limpo.
+  const responseBody = await response.json();
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+});
